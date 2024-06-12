81bea84cb8cc8cc726cfe425561d86a096461d46
chandan <chandankoranga728@gmail.com>
2024-06-12 19:55:19 +0530

    task_done

---
+++ appwrite/Mega_blog/src/components/container/Container.jsx
@@ -0,0 +1,10 @@
+import { Container } from 'postcss'
+import React from 'react'
+ // container accept properties as  children 
+function Container() {
+  return (
+    <div>Container</div>
+  )
+}
+
+export default Container
---
+++ appwrite/Mega_blog/src/components/index.js
@@ -0,0 +1,7 @@
+ // this is used for exporting  Header ans Footer
+  import Header  from  "./Header/Header";
+  import Footer  from  "./Footer/Footer";
+  export {
+    Header,
+    Footer
+  }
